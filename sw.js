be33576810952699064f53216154053497b04bbd
hurcelltest-max <hurcelltest@gmail.com>
2026-04-26 18:55:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-const CACHE_NAME = 'dunyadaturkiye-v1';
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll([
-      '/', '/index.html', '/styles.css', '/app.js', '/manifest.json'
-    ]))
-  );
-  self.skipWaiting();
-});
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()) );
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
-});
+self.addEventListener('fetch', function(event) {});
